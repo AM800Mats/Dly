ae96b5cc3880224275c8525f83a91484906c06a0
AM800Mats <am800mats@gmail.com>
2024-03-12 10:35:46 +0100

    embedding test iframe added

--- dailyle/src/App.js
+++ dailyle/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1>Dailyle</h1>
+        <iframe class="iFrame" src="https://worldle.teuteuf.fr/" title="Embedded Website"></iframe>
+      </body>
     </div>
   );
 }
