651e2606df303eacae2431d1ebb32cc02258d8ca
AM800Mats <am800mats@gmail.com>
2024-03-15 00:39:32 +0100

    -Added Game selection for 5 mins, tried centering a div for 2h, -Made a cool background animation and a preliminary Logo

--- dailyle/src/App.tsx
+++ dailyle/src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   const iFrame = useRef<HTMLIFrameElement>(null);
   const [text, setText] = useState("");
 
+  const [selectedGame, setSelectedGame] = useState("Worldle");
+  const [gameUrl, setGameUrl] = useState("https://worldle.teuteuf.fr/");
+
   /**
    * Handles the click event and fetches data from the server.
    */
@@ -20,21 +23,61 @@ function App() {
     }
   };
 
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGame(event.target.value);
+    switch (event.target.value) {
+      case "Worldle":
+        setGameUrl("https://worldle.teuteuf.fr/");
+        break;
+      case "Costcodle":
+        setGameUrl("https://costcodle.com/");
+        break;
+      case "Tradle":
+        setGameUrl("https://games.oec.world/en/tradle/");
+        break;
+      case "NYT Wordle":
+        setGameUrl("https://www.nytimes.com/games/wordle/index.html");
+        break;
+      case "Flagle":
+        setGameUrl("https://flagle-game.com/");
+        break;
+      case "Foodguessr":
+        setGameUrl("https://foodguessr.com/");
+        break;
+      case "Unzoomed":
+        setGameUrl("https://unzoomed.com/");
+        break;
+      default:
+        setGameUrl("https://worldle.teuteuf.fr/");
+    }
+  };
+
   return (
     <div className="App">
-      <body>
+      <body className="background">
         <h1>Dailyle</h1>
         <p>{text}</p>
         <iframe
           ref={iFrame}
           className="iFrame"
           id="iFrame"
-          src="https://worldle.teuteuf.fr/"
+          src={gameUrl}
           title="Embedded Website"
         ></iframe>
-        <button className="button" onClick={handleClick}>
-          Update Text
-        </button>
+        <div className='optionsField'>
+          <button className="button" onClick={handleClick}>
+            Update Text
+          </button>
+          <select value={selectedGame} onChange={handleSelectChange}>
+            <option value="Worldle">Worldle</option>
+            <option value="Costcodle">Costcodle</option>
+            <option value="Tradle">Tradle</option>
+            <option value="NYT Wordle">NYT Wordle</option>
+            <option value="Flagle">Flagle</option>
+            <option value="Foodguessr">Foodguessr</option>
+            <option value="Unzoomed">Unzoomed</option>
+          </select>
+        </div>
         <Test />
       </body>
     </div>
--- server/index.js
+++ server/index.js
@@ -10,17 +10,19 @@ app.use(cors());
 
 app.get('/scrape', async (req, res) => {
   console.log('Scraping data...')
+  const startTime = Date.now();
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://worldle.teuteuf.fr/');
 
-  // Replace the XPath query with the actual query to select your element
   const el = await page.$('.text-green-600');
   const text = await el.getProperty('textContent');
   const rawText = await text.jsonValue();
   console.log('Browser clossing', rawText);
   await browser.close();
   console.log('Browser closed, Data scraped:', rawText);
+  const endTime = Date.now();
+  console.log('Time elapsed:', endTime - startTime);
   res.send(rawText);
 });
 
