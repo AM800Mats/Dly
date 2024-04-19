2b77eb234bfbd609b5be919343da6a47a27714b8
AM800Mats <am800mats@gmail.com>
2024-04-19 15:20:40 +0200

    Remove background.js, update manifest.json, and add server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
