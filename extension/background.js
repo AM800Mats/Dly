a9d5d32f178746c9f57618b56f0a7141b0a413ad
AM800Mats <am800mats@gmail.com>
2024-04-29 15:33:57 +0200

    Refactor code to fetch scores from different costcodle and foodguessr, both working flawlessly (hehe)

--- extension/background.js
+++ extension/background.js
@@ -1,36 +1,50 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    chrome.tabs.query({active: true}, function(tabs) {
-        if (tabs.length === 0) {
-            console.log('No active tabs');
-            return;
+  if (request.action !== 'getScore') {
+      return;
+  }
+  chrome.tabs.query({active: true}, function(tabs) {
+    if (tabs.length === 0) {
+      console.log('No active tabs');
+      return;
+    }
+    let tab = tabs[0]; // the current tab
+    switch (true) {
+    case tab.url.includes("costcodle.com"):
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: function() {
+          let checkElement = document.getElementsByClassName('guess-direction-container animate__flipInX guess-win')[0];
+          if (checkElement){
+            let guessNum = checkElement.parentElement.id;
+            console.log('Sending message to popup.js:' + guessNum);
+            chrome.runtime.sendMessage({score: guessNum});
+          }
+          else {
+            let guessNum = 'DNF';
+            console.log('Sending message to popup.js:' + guessNum);
+            chrome.runtime.sendMessage({score: guessNum});
+          }
         }
-        let tab = tabs[0]; // the current tab
-        switch (true) {
-        case tab.url.includes("costcodle.com"):
-          chrome.scripting.executeScript({  
-            target: { tabId: tab.id },
-            files: ['Costco.js']
-          });
-          break;
-        case tab.url.includes("foodguessr.com/final-results"):
-          chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: function() {
-                let scoreDiv = document.querySelector('.select-none.text-black');
-                let clonedScoreDiv = scoreDiv.cloneNode(true);
-                let childElements = clonedScoreDiv.querySelectorAll('*');
-                for (let i = 0; i < childElements.length; i++) {
-                    childElements[i].parentNode.removeChild(childElements[i]);
-                }
-                let score = clonedScoreDiv.textContent;
-                console.log(score);
-                console.log('Sending message to background.js');
-                chrome.runtime.sendMessage({score: score});
-            }
-          });
-          break;
-        default:
-          console.log('No matching scripts for this URL');
-      }
-    });
+      });
+      break;
+    case tab.url.includes("foodguessr.com/final-results"):
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: function() {
+          let scoreDiv = document.querySelector('.select-none.text-black');
+          let clonedScoreDiv = scoreDiv.cloneNode(true);
+          let childElements = clonedScoreDiv.querySelectorAll('*');
+          for (let i = 0; i < childElements.length; i++) {
+            childElements[i].parentNode.removeChild(childElements[i]);
+          }
+          let score = clonedScoreDiv.textContent;
+          console.log('Sending message to popup.js');
+          chrome.runtime.sendMessage({score: score});
+        }
+      });
+      break;
+      default:
+        console.log('No matching scripts for this URL');
+    }
+  });
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,37 @@
 let a = 0;
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.runtime.sendMessage({tabId: tabs[0].id});
-  });
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    let startButton = document.getElementById('getScoreFromPage');
-    startButton.addEventListener('click', () => {
-      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        // The score is in request.score
-        if (request.score) {
-            console.log(request.score);
-            document.getElementById("score").textContent = request.score;
-          }
-      });
+/**
+ * Event listener for the DOMContentLoaded event.
+ * @param {Event} event - The DOMContentLoaded event object.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {  
+  /**
+   * Click event listener for the start button.
+   */
+  let startButton = document.getElementById('getScoreFromPage');
+  startButton.addEventListener('click', () => {
+    /**
+     * Query the active tab and send a message to the background script to get the score.
+     */
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.runtime.sendMessage({tabId: tabs[0].id, action: "getScore"});
     });
+
+    /**
+     * Event listener for the runtime message.
+     * @param {Object} request - The message request object.
+     * @param {Object} sender - The sender object.
+     * @param {Function} sendResponse - The function to send a response.
+     */
+    let listener = function(request, sender, sendResponse) {
+      if (request.score) {
+        a++;
+        console.log('received score' + request.score + sender + a);
+        document.getElementById("score").textContent = request.score;
+        chrome.runtime.onMessage.removeListener(listener);
+      }
+    };
+    chrome.runtime.onMessage.addListener(listener);
+    
+  });
 });
 
