ec47c3b1ec115183b1b3cba6e68e9aa37af1fe21
AM800Mats <am800mats@gmail.com>
2024-04-29 15:53:36 +0200

    Refactor score retrieval logic in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,30 @@
+
+function getCostcoScore() {
+  let checkElement = document.getElementsByClassName('guess-direction-container animate__flipInX guess-win')[0];
+  if (checkElement) {
+    let guessNum = checkElement.parentElement.id;
+    console.log('Sending message to popup.js:' + guessNum);
+    chrome.runtime.sendMessage({ score: guessNum });
+  }
+  else {
+    let guessNum = 'DNF';
+    console.log('Sending message to popup.js:' + guessNum);
+    chrome.runtime.sendMessage({ score: guessNum });
+  }
+}
+
+  function getFoodguessrScore() {
+    let scoreDiv = document.querySelector('.select-none.text-black');
+    let clonedScoreDiv = scoreDiv.cloneNode(true);
+    let childElements = clonedScoreDiv.querySelectorAll('*');
+    for (let i = 0; i < childElements.length; i++) {
+      childElements[i].parentNode.removeChild(childElements[i]);
+    }
+    let score = clonedScoreDiv.textContent;
+    console.log('Sending message to popup.js');
+    chrome.runtime.sendMessage({ score: score });
+  }
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action !== 'getScore') {
       return;
@@ -8,39 +35,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       return;
     }
     let tab = tabs[0]; // the current tab
+
+    //switch cases for each url of supported games
     switch (true) {
     case tab.url.includes("costcodle.com"):
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: function() {
-          let checkElement = document.getElementsByClassName('guess-direction-container animate__flipInX guess-win')[0];
-          if (checkElement){
-            let guessNum = checkElement.parentElement.id;
-            console.log('Sending message to popup.js:' + guessNum);
-            chrome.runtime.sendMessage({score: guessNum});
-          }
-          else {
-            let guessNum = 'DNF';
-            console.log('Sending message to popup.js:' + guessNum);
-            chrome.runtime.sendMessage({score: guessNum});
-          }
-        }
+        func: getCostcoScore
       });
       break;
     case tab.url.includes("foodguessr.com/final-results"):
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: function() {
-          let scoreDiv = document.querySelector('.select-none.text-black');
-          let clonedScoreDiv = scoreDiv.cloneNode(true);
-          let childElements = clonedScoreDiv.querySelectorAll('*');
-          for (let i = 0; i < childElements.length; i++) {
-            childElements[i].parentNode.removeChild(childElements[i]);
-          }
-          let score = clonedScoreDiv.textContent;
-          console.log('Sending message to popup.js');
-          chrome.runtime.sendMessage({score: score});
-        }
+        func: getFoodguessrScore
       });
       break;
       default:
